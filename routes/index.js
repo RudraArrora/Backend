bd5d8755e703b4fc2903c7dee614a6691bcdaee7
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-07 12:59:50 +0530

    Home functions

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const router  = express.Router();
+
+const homeController =require('../controllers/home');
+
 console.log('router loaded');
+
+router.get('/',homeController.home);
+
 module.exports = router;
