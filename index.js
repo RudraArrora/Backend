18e2895646baf150a533039cf71e9f9568bb76ca
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-08 00:08:09 +0530

    Setting DB in index file

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require ('express');
 const app =express();
 const port = 8000;
 // use express router
+const db = require("./config/moongose");
+app.use(express.urlencoded());
 app.use('/',require('./routes'));
 
 
 app.set('view engine','ejs');
 app.set('views','./views')
+
+
+
 app.listen(port,function(err){
     if (err){
         console.log ('Error:',err);
