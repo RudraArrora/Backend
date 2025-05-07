0a67a9342567b8e6e9d501ae10f1a2652b61a2d9
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-07 07:39:32 +0530

    Running the port

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ const app =express();
 const port = 8000;
 
 
+app.listen(port,function(err){
+    if (err){
+        console.log ('Error:',err);
+    }
+    else{
+        console.log ("Running on server port : ",port);
+    }
+})
