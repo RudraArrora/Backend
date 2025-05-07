06c6eb2047735152bda5f24f72a409b2c7d77230
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-08 00:09:57 +0530

    User Router

---
+++ routes/user.js
@@ -0,0 +1,9 @@
+const express= require('express');
+const router = express.Router();
+
+const userController =require('../controllers/user_controller');
+
+router.get('/signIn',userController.signIn);
+router.get('/signUp',userController.signUp);
+router.post('/create',userController.create);
+module.exports=router;
