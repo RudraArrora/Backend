aeb036c7cb503d6354f50d3ef2a35be4ebc8d75b
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-10 23:20:26 +0530

    Creating Sessions In Index Files

--- index.js
+++ index.js
@@ -4,20 +4,33 @@ const port = 8000;
 // use express router
 const db = require("./config/moongose");
 //////////////////////////
-const passport= require('passport')
+const session = require('express-session');
+const passport = require("passport");
 const passportLocal = require("./config/passport");
 /////////////////////
 
 
 app.use(express.urlencoded());;
-app.use('/',require('./routes'));
+
 
 
 app.set('view engine','ejs');
 app.set('views','./views')
 
+app.use(session({
+    name:'codiel',
+    secret: 'blahsomething',
+    saveUnitialized:false,
+    resave : false,
+    cookie:{
+        maxAge : (1000*60*100)
+    }
+}))
 
+app.use(passport.initialize())
+app.use(passport.session())
 
+app.use("/", require("./routes"));
 app.listen(port,function(err){
     if (err){
         console.log ('Error:',err);
