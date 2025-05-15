901a21c4a41e87d999b9e6e4417e34246bed9385
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-15 13:45:30 +0530

    Post router

---
+++ routes/post.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const postController = require('../controllers/post');
+const passport = require("passport");
+router.post('/create',passport.checkAuthentication,postController.create);
+module.exports = router;
