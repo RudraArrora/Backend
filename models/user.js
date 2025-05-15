a2df00463d180bdfff216b694c43105e7e920ea5
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-15 13:47:32 +0530

    User Schema

---
+++ models/user.js
@@ -0,0 +1,23 @@
+const moongose = require('mongoose');
+const userSchema = new moongose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  }
+},{
+    timeStamps : true
+});
+
+const User=moongose.model('User',userSchema);
+
+
+module.exports=User;
