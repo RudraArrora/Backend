6d2e8b4022c02c172308fcfea84373479d5700e2
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-15 13:46:12 +0530

    Route Authentication

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,15 @@
 const express= require('express');
 const router = express.Router();
-
+const passport= require('passport');
 const userController =require('../controllers/user_controller');
-
+router.get("/profile",passport.checkAuthentication, userController.pro);
 router.get('/signIn',userController.signIn);
 router.get('/signUp',userController.signUp);
 router.post('/create',userController.create);
+router.post(
+  "/createSession",
+  passport.authenticate("local", { failureRedirect: "/users/signIn" }),
+  userController.createSession
+);
+router.get('/signOut',userController.destroy)
 module.exports=router;
