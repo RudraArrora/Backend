63f745fe92bfd8ff18087b9fb483e7bb62e5c306
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-17 22:42:57 +0530

    Destroy Routes

--- routes/post.js
+++ routes/post.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const postController = require('../controllers/post');
 const passport = require("passport");
 router.post('/create',passport.checkAuthentication,postController.create);
+router.get('/destroy/:id',passport.checkAuthentication,postController.destroy);
 module.exports = router;
