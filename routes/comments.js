3791c6b6217353326486825c4cdcb7200cbb9031
Rudra Arora <arorarudra.it11@gmail.com>
2025-05-18 15:05:44 +0530

    Destroy The comment

--- routes/comments.js
+++ routes/comments.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const commentController = require("../controllers/comment");
 const passport = require("passport");
 router.post("/create", passport.checkAuthentication, commentController.create);
+router.get("/destroy/:id", passport.checkAuthentication,commentController.destroy);
 module.exports = router;
